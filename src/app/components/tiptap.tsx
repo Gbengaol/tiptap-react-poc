5e3d1161cae7df6d7ddaaf6d9d2dea9252a4fe28
Olufeyimi Gbenga <gbengacodes@gmail.com>
2024-09-27 00:15:30 +0100

    Remove unused extensions

--- src/app/components/tiptap.tsx
+++ src/app/components/tiptap.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Color } from "@tiptap/extension-color";
-import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
 import { EditorProvider } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
@@ -16,8 +14,7 @@ import { ToolBar } from "./toolbar";
 import { cn } from "@/lib/utils";
 
 const extensions = [
-  Color.configure({ types: [TextStyle.name, ListItem.name] }),
-  TextStyle.configure({ types: [ListItem.name] }),
+  TextStyle,
   StarterKit.configure({
     bulletList: {
       keepMarks: true,
